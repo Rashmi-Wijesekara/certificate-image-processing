046fb7e7cd59c08520fefe8937ee7eee4aaa9b7c
Rashmi <rashmiwijesekara1999@gmail.com>
2023-06-06 11:39:24 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CertificateImageProcessing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
